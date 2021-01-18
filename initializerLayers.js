0f73564dcfd4401b251c051dd1c40a674127ad1e
Larkoss <klarko01@ucy.ac.cy>
2021-01-18 23:33:11 +0200

    Bugfix: Add map symbols/text above the buildings layers

--- initializerLayers.js
+++ initializerLayers.js
@@ -1,13 +1,18 @@
 //Attach created layers
 map.on('load', function () {
-    // Insert the layer beneath any symbol layer.
-    //Symbol layers are layers of text/icons
+    
     var layers = map.getStyle().layers;
 
-    var labelLayerId;
+    //the addLayer method takes 2 arguments: the layer as an object, and a string
+    // representing another layer's name. if the other layer
+    // exists in the stylesheet already, the new layer will be positioned
+    // right before that layer in the stack, making it possible to put
+    // 'overlays' anywhere in the layer stack.
+    // Insert the layer beneath the first symbol layer.
+    var firstSymbolId;
     for (var i = 0; i < layers.length; i++) {
         if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-            labelLayerId = layers[i].id;
+            firstSymbolId = layers[i].id;
             break;
         }
     }
@@ -17,5 +22,6 @@ map.on('load', function () {
     //map.addLayer(layerTerrainLines);
     //map.addLayer(layerKatoArodes);
     //map.addLayer(layerStRaphaelMarina);
-    //map.addLayer(layerBuildingsData);
+    map.addLayer(layerBuildings, firstSymbolId); 
+    
 });
--- layerBuildings.js
+++ layerBuildings.js
@@ -2,21 +2,19 @@ map.on('load', function () {
     map.addSource('buildings-data', {
         type: 'geojson',
         data: '/buildingsWGS84.geojson',
-        //cluster: true
-
     });
-
-    map.addLayer({
-        'id': 'buildings-data',
-        'type': 'fill',
-        'source': 'buildings-data',
-        'layout': {
-            //'icon-allow-overlap': 'false'
-        },
-        'paint': {
-            'fill-color': '#34f7d7',
-            'fill-opacity': 0.8
-        }
-    });
-
 });
+const layerBuildings = {
+    'id': 'buildings-data',
+    'type': 'fill',
+    'source': 'buildings-data',
+    'layout': {
+        //'icon-allow-overlap': 'false'
+    },
+    'paint': {
+        'fill-color': '#34f7d7',
+        'fill-opacity': 0.8
+    }
+};
+
+
