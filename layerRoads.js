69b2c3f5e8c0962f0a9afa4ca9b980001169ce55
Larkoss <klarko01@ucy.ac.cy>
2021-12-02 02:12:49 +0200

    Add Road Data and appear them in the map

---
+++ layerRoads.js
@@ -0,0 +1,35 @@
+map.on('load', function () {
+    map.addSource('roads', {
+        type: 'geojson',
+        data: './roadnetwork_original.geojson',
+        //buffer: 0,
+        //tolerance: 3.5
+    });
+});
+const layerRoads = {
+    'id': 'roads',
+    'type': 'line',
+    'source': 'roads',
+    'layout': {
+        //'icon-allow-overlap': true
+    },
+    'paint': {
+        'line-color': '#ff69b4',
+        'line-width': 1
+    }
+};
+
+const layerRoadsHighlighted = {
+    'id': 'roads-highligted',
+    'type': 'line',
+    'source': 'roads',
+    'layout': {
+        //'icon-allow-overlap': true
+    },
+    'paint': {
+        'line-color': '#ae1ed6',
+        'line-width': 3
+    },
+    'filter': ['in', 'id', '']
+};
+
